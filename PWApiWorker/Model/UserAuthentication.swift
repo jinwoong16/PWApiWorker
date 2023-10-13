887b831170cf99fb4513317e4f0377ee75942411
jinwoong <baseball0020@gmail.com>
2023-10-14 02:56:00 +0900

    comment: add comments for clarity.
    Related to #2

--- PWApiWorker/Model/UserAuthentication.swift
+++ PWApiWorker/Model/UserAuthentication.swift
@@ -7,10 +7,15 @@
 
 import Foundation
 
+/// An object where user information on the API server is located.
 public struct UserAuthentication {
+    /// A user id from the api server.
     var userId: String
+    
+    /// A user secret from the api server.
     var userSecret: String
     
+    /// An encrypted string based on base64 encoding.
     var encryptedString: String? {
         String(
             format: "%@:%@",
