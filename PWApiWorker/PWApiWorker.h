06f06dbf35ad6a52a13909b1d9545fdb97b8b7ec
jinwoong <baseball0020@gmail.com>
2023-10-13 22:33:15 +0900

    Initial Commit

---
+++ PWApiWorker/PWApiWorker.h
@@ -0,0 +1,18 @@
+//
+//  PWApiWorker.h
+//  PWApiWorker
+//
+//  Created by jinwoong Kim on 10/13/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PWApiWorker.
+FOUNDATION_EXPORT double PWApiWorkerVersionNumber;
+
+//! Project version string for PWApiWorker.
+FOUNDATION_EXPORT const unsigned char PWApiWorkerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PWApiWorker/PublicHeader.h>
+
+
