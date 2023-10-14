a0e2cd3964bbe2c0fb93f241d2a2571efdbd21b8
jinwoong <baseball0020@gmail.com>
2023-10-14 19:12:24 +0900

    feat: add `TokenContainable` protocol
    Related to #4

---
+++ PWApiWorker/Protocols/TokenContainable.swift
@@ -0,0 +1,12 @@
+//
+//  TokenContainable.swift
+//  PWApiWorker
+//
+//  Created by jinwoong Kim on 10/14/23.
+//
+
+import Foundation
+
+public protocol TokenContainable: Decodable {
+    var accessToken: String { get }
+}
