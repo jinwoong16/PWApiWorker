5af86a038846d50fc8941c96c8fb2a987152bb08
jinwoong <baseball0020@gmail.com>
2023-10-14 16:15:02 +0900

    feat: add response body form from vito
    Related #4

---
+++ PWApiWorker/Model/VitoApi/VitoTokenBody.swift
@@ -0,0 +1,18 @@
+//
+//  VitoTokenBody.swift
+//  PWApiWorker
+//
+//  Created by jinwoong Kim on 10/14/23.
+//
+
+import Foundation
+
+struct VitoTokenBody: Decodable {
+    var accessToken: String
+    var expireAt: String
+    
+    enum CodingKeys: String, CodingKey {
+        case accessToken = "access_token"
+        case expireAt = "expire_at"
+    }
+}
