46406900e474585e2e6e7565cadad782ae6e8783
jinwoong <baseball0020@gmail.com>
2023-10-14 22:15:15 +0900

    feat: add `VITOServiceError`
    - it indicates a situation that could have occurred because of the Vito service.

---
+++ PWApiWorker/Model/Error/VITOServiceError.swift
@@ -0,0 +1,12 @@
+//
+//  VITOServiceError.swift
+//  PWApiWorker
+//
+//  Created by jinwoong Kim on 10/14/23.
+//
+
+import Foundation
+
+enum VITOServiceError: Error {
+    case fileNotFound
+}
---
+++ PWApiWorker/NetworkService/VITOApiService.swift
@@ -0,0 +1,8 @@
+//
+//  VITOApiService.swift
+//  PWApiWorker
+//
+//  Created by jinwoong Kim on 10/14/23.
+//
+
+import Foundation
